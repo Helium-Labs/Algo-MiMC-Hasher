dccab9395262d19c79821bb61357c561bc9de2ff
mangoplane <devmon1@protonmail.com>
2024-08-21 17:03:21 +1000

    Remove unused code

--- copyright-notices/README.md
+++
@@ -1,3 +0,0 @@
-# Copyright Notices
-
-The files in the same directory as this README are copyright notices that must be included to satisfy the license terms of the copied code.
--- src/util.ts
+++ src/util.ts
@@ -16,21 +16,6 @@ export function chunks(input: Uint8Array, size: number): Uint8Array[] {
   return result
 }
 
-export function bigintToArray(n: number, k: number, x: bigint): string[] {
-  let mod: bigint = 1n
-  for (let idx = 0; idx < n; idx++) {
-    mod = mod * 2n
-  }
-
-  const ret: bigint[] = []
-  let xTemp: bigint = x
-  for (let idx = 0; idx < k; idx++) {
-    ret.push(xTemp % mod)
-    xTemp = xTemp / mod
-  }
-  return ret.map((x) => x.toString())
-}
-
 export function pad(arr: Uint8Array, width = 1024): { length: number, padded: Uint8Array } {
   // pad the uint8array to the width with 0s
   const padded = new Uint8Array(width)
@@ -53,41 +38,6 @@ export function leftPadAsMultiple(arr: Uint8Array, multiple = 32): { length: num
   const missingLength = multiple - arr.length % multiple
   const width = arr.length + missingLength
   return leftPad(arr, width)
-}
-
-
-/**
- * bigIntToBytes converts a BigInt to a big-endian Uint8Array for encoding.
- * @param bi - The bigint to convert.
- * @param size - The size of the resulting byte array.
- * @returns A byte array containing the big-endian encoding of the input bigint
- */
-export function bigIntToBytes(bi: bigint | number, size: number) {
-  let hex = bi.toString(16);
-  // Pad the hex with zeros so it matches the size in bytes
-  if (hex.length !== size * 2) {
-    hex = hex.padStart(size * 2, '0');
-  }
-  const byteArray = new Uint8Array(hex.length / 2);
-  for (let i = 0, j = 0; i < hex.length / 2; i++, j += 2) {
-    byteArray[i] = parseInt(hex.slice(j, j + 2), 16);
-  }
-  return byteArray;
-}
-
-/**
- * bytesToBigInt produces a bigint from a binary representation.
- *
- * @param bytes - The Uint8Array to convert.
- * @returns The bigint that was encoded in the input data.
- */
-export function bytesToBigInt(bytes: Uint8Array) {
-  let res = BigInt(0);
-  const buf = Buffer.from(bytes);
-  for (let i = 0; i < bytes.length; i++) {
-    res = BigInt(Number(buf.readUIntBE(i, 1))) + res * BigInt(256);
-  }
-  return res;
 }
 
 export function base64ToBase64url(base64: string): string {
