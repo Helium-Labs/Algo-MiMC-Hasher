7314f0240f47568c2d8377d7ca813f40f3f92fbd
mangoplane <devmon1@protonmail.com>
2024-08-22 15:45:51 +1000

    Remove node polyfilling from Rollup - node only package

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,8 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
 import typescript from 'rollup-plugin-typescript2'
-import terser from '@rollup/plugin-terser'
 
 export default [
-  // Browser config
   {
     input: 'src/index.ts',
     output: [
@@ -17,11 +14,6 @@ export default [
     plugins: [
       json(),
       commonjs(),
-      nodeResolve({
-        preferBuiltins: true,
-        browser: true,
-        modulesOnly: false
-      }),
       typescript({
         tsconfig: './tsconfig.esm.json',
         useTsconfigDeclarationDir: true,
