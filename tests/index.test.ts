d6c57f31dc31ae742d564d616696928e99c5df88
mangoplane <devmon1@protonmail.com>
2024-08-23 11:40:13 +1000

    Fix/simplify build process, and add algosdk as a peer dependency

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,19 +1,23 @@
 import { getTransactionSignerFromMnemonic } from '../src/util';
 import algosdk from 'algosdk';
 import 'dotenv/config'
-import { fundAccount, getAlgokitTestkit } from 'algokit-testkit'
 import { MIMCClient } from 'algo-mimc-hasher'
 
 const TEST_MNEMONIC: string = process.env.TEST_MNEMONIC!
 
+const getAlgod = async () => {
+  // Define the Algorand node connection parameters
+  const algodToken = '' // free service does not require tokens
+  const algodServer = 'https://testnet-api.algonode.cloud'
+  const algodPort = 443
+
+  // Create an instance of the algod client
+  return new algosdk.Algodv2(algodToken, algodServer, algodPort)
+}
+
 test("MIMC Constant as hex string", async () => {
-  console.warn("MIMC of small text 'hello'")
-
-  const { algod } = await getAlgokitTestkit()
+  const algod = await getAlgod()
   const signer = getTransactionSignerFromMnemonic(TEST_MNEMONIC);
-  const account = algosdk.mnemonicToSecretKey(TEST_MNEMONIC)
-
-  await fundAccount(account.addr, 1e9)
 
   const mimcClient = new MIMCClient(algod, signer)
 
@@ -27,13 +31,10 @@ test("MIMC Constant as hex string", async () => {
 }, 120_000)
 
 test("MIMC of large RSA signing modulus (n)", async () => {
-  console.warn("MIMC of large RSA signing modulus (n)")
   const googleSigningModulus = "nzGsrziOYrMVYMpvUZOwkKNiPWcOPTYRYlDSdRW4UpAHdWPbPlyqaaphYhoMB5DXrVxI3bdvm7DOlo-sHNnulmAFQa-7TsQMxrZCvVdAbyXGID9DZYEqf8mkCV1Ohv7WY5lDUqlybIk1OSHdK7-1et0QS8nn-5LojGg8FK4ssLf3mV1APpujl27D1bDhyRb1MGumXYElwlUms7F9p9OcSp5pTevXCLmXs9MJJk4o9E1zzPpQ9Ko0lH9l_UqFpA7vwQhnw0nbh73rXOX2TUDCUqL4ThKU5Z9Pd-eZCEOatKe0mJTpQ00XGACBME_6ojCdfNIJr84Y_IpGKvkAEksn9w"
 
-  const { algod } = await getAlgokitTestkit()
+  const algod = await getAlgod()
   const signer = getTransactionSignerFromMnemonic(TEST_MNEMONIC);
-  const account = algosdk.mnemonicToSecretKey(TEST_MNEMONIC)
-  await fundAccount(account.addr, 1e9)
 
   const mimcClient = new MIMCClient(algod, signer)
 
