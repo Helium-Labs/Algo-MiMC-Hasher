4bfcec232c9d1157af198ebe77b73d5d30a49bd0
mangoplane <devmon1@protonmail.com>
2024-08-27 21:59:18 +1000

    fix: remove duplicate test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -63,21 +63,6 @@ test("MIMC of OIDC issuer", async () => {
   await mimcClient.verifyMimcHash(data)
 }, 120_000)
 
-test("MIMC of OIDC issuer", async () => {
-  const algod = await getAlgod()
-  const signer = getTransactionSignerFromMnemonic(TEST_MNEMONIC);
-
-  const mimcClient = new MIMCClient(algod, signer)
-
-  await mimcClient.createMimcApp()
-  const dataAsBuf = Buffer.from("https://accounts.google.com", 'utf8')
-  const data = Uint8Array.from(dataAsBuf)
-
-  await mimcClient.initialize(data)
-  await mimcClient.multimimc7(data)
-  await mimcClient.verifyMimcHash(data)
-}, 120_000)
-
 test("Expect failure: invalid MIMC hash should fail", async () => {
   const algod = await getAlgod()
   const signer = getTransactionSignerFromMnemonic(TEST_MNEMONIC);
